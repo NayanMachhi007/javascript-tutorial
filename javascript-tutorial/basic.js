fd454ffb3150d8fdd6030b8e546973312dc4cff7
NayanMachhi007 <143252087+NayanMachhi007@users.noreply.github.com>
2023-12-13 12:16:28 +0000

    new code updated

--- javascript-tutorial/basic.js
+++ javascript-tutorial/basic.js
@@ -1,2 +1,13 @@
-console.log("hello world");
-console.log("Hello nayan");
+const accontid = 107;
+let accoutnEmail = "nayankumarmachhi@gmail.com";
+var accountPass = "Nayan";
+// accontid = 133; // not allowed
+accountCity = "Gujarat";
+
+accoutnEmail = "nayankumarmachhi05@gmail.com";
+accountPass = "NayanMac";
+accountCity = "Mumbai";
+
+console.log(accontid);
+
+console.table([accoutnEmail, accountPass, accountCity]);
