44a892de84910267ccf73fae1f2cf8cc3fa0c8f2
NayanMachhi007 <143252087+NayanMachhi007@users.noreply.github.com>
2023-12-13 13:05:01 +0000

    data type

---
+++ javascript-tutorial/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js. not browser 
+
+console.log(3+3); // Code readability should be high
+
+
+
+
+// number = 2
+// bigint
+// string = ""
+// boolean = true/false
+// null => tandalone value
+// undefined
+// symbol => unique 
+
+
+
+
+// object 
+
+console.log(typeof null);
