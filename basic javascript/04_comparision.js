985071793374a611a74d49afa7249d95a9078a64
NayanMachhi007 <143252087+NayanMachhi007@users.noreply.github.com>
2023-12-23 14:12:52 +0000

    new updated

---
+++ basic javascript/04_comparision.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+// console.log(2 > 1);
+// console.log(2 < 3);
+// console.log(2 >= 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+// console.log(null > 2);
+// console.log(null >= "2");
+// console.log(null = 0);
+// console.log(null <= "2");
+
+// console.log(undefined > 2);
+// console.log(undefined < "2");
+// console.log(undefined == 2);
+// console.log(undefined >= "2");
+
+
+// === (that mean we use as a strict check)
+
+let Number1 = 2;
+let Number2 = "2";
+
+console.log(Number1 == Number2); // when you check with double equal that mean they just check with number 
+
+console.log(Number1 === Number2);// When you check with triple equal that mean they checked with also data type
