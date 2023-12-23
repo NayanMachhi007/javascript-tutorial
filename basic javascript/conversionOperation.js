d4aa63c3045e4c6b74af86ebc6b104f23fcc675f
NayanMachhi007 <143252087+NayanMachhi007@users.noreply.github.com>
2023-12-23 13:49:37 +0000

    new update

--- basic javascript/conversionOperation.js
+++ basic javascript/conversionOperation.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 let a = "3abc3";
 let b = "Nayan";
 let c = Number(a);
@@ -8,3 +11,23 @@ console.log(c);
 // "33" => 33
 // "33abc" => NaN
 // true => 1 false => 0
+
+
+/************************************** Operation ********************************************/
+
+let Value = 3;
+let NegValue = -Value;
+
+//console.log(NegValue);
+//console.log(3+3);
+//console.log(3*3);
+//console.log(3+3);
+//console.log(3**3);
+//console.log(3/3);
+//console.log(3%3);
+
+let Str1 = "Hello";
+let Str2 = " Nayan";
+let Str3 = Str1 + Str2;
+
+console.log(Str3);
